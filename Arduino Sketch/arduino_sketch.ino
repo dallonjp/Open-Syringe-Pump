c05128f22bbceb3a3e3ed9c995b1e3e593de1652
Dallon Penney <52696430+dallonjp@users.noreply.github.com>
2020-08-21 19:00:04 -0700

    Update arduino_sketch.ino

--- Arduino Sketch/arduino_sketch.ino
+++ Arduino Sketch/arduino_sketch.ino
@@ -1,23 +1,31 @@
 
+
 #include <AccelStepper.h>
+#include <FlexiTimer2.h>
 #define dirPin 2 //digital pin 2 on arduino connects to dir pin on A4988
 #define stepPin 3 //digital pin 3 on arduino connects to step pin on A4988
 #define homeButton 5 //digital pin 5 on arduino connects to normally closed (green) wire on endstop
 #define motorInterfaceType 1
 
-char dirinput[1];
-long positionX;
+long steps;
 long init_homing=-1;
+long stepto;
 long mspeed;
 long maccel;
-int mdir;
+long mdir;
 long mpos;
+long interval;
+long elapsed=0;
+long defaultrate;
+long defaultaccel;
+long timenow;
+long timethen;
 
 int msg_length;
 bool connected =false;
 bool datarvd = false;
-char  userInput[17]; 
-char userInput2[6];//={'g','0','0','0','0','0'};
+char  userInput[20]; 
+char userInput2[6];
 
 // Create a new instance of the AccelStepper class:
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
@@ -27,24 +35,21 @@ void setup()
     //set up serial connection
     Serial.begin(9600);
     Serial.setTimeout(1000); 
-    //set defaults
-    stepper.setCurrentPosition(0);
-    stepper.setMaxSpeed(3000);
-    stepper.setAcceleration(2000);
-    
+    pinMode(dirPin,OUTPUT);
+    pinMode(stepPin,OUTPUT);
 }
 void loop()
 {    
     while(Serial.available()>0){
       
       char handshake = Serial.read();
-      //Serial.print(handshake);
       switch (handshake){
         case 'a':
         //connect to app
           delay(100);
           Serial.println("hello winform");
           delay(100);
+          getdefaults();
         break;
         case 'b':
         //home sequence
@@ -58,6 +63,10 @@ void loop()
         //change acceleration
           changeaccel();
         break;
+        case 'f':
+        //timed injection
+        timedaction();
+        break;
         case 'd':
         //stop current operation
         break;
@@ -68,22 +77,54 @@ void loop()
       }
     }
 }
+void timedaction(){
+  while (!datarvd){
+    if(Serial.readBytes(userInput,20)==20){
+      for (int i=1; i<10; i++){
+        steps=steps*10+userInput[i]-'0';
+      }
+      for (int j=11;j<20;j++){
+          interval=interval*10+userInput[j]-'0';
+        }
+      elapsed=0;
+      stepto=stepper.currentPosition()-1;
+      stepper.setMaxSpeed(10000);
+      stepper.setAcceleration(5000);
+      while (elapsed<interval){
+        if (Serial.available()>0){
+          interval=0;
+          steps=0;
+          break;
+        }else{
+          delay(steps);
+          stepper.moveTo(stepto);  
+          stepper.run();
+          elapsed=elapsed+steps;
+          stepto=stepto-1;
+        }
+      }
+      interval=0;
+      steps=0;
+      datarvd=true;
+    }
+  }
+  datarvd=false;
+}
+
 void moveto(){
   while (!datarvd){
     if (Serial.readBytes(userInput,17)==17){
       for(int i=1; i<8;i++){
-        positionX=positionX*10+userInput[i]-'0';
-        //Serial.println(userInput[i]);
+        steps=steps*10+userInput[i]-'0';
         }
         for (int j=9;j<15;j++){
           mspeed=mspeed*10+userInput[j]-'0';
         }
-        dirinput[0]=char(userInput[16]);
-        mdir = atoi(dirinput);      
+        mdir = userInput[16]-'0';      
         if(mdir==1){
-          mpos = positionX*1;
+          mpos = steps*1;
         }else if (mdir==2){
-          mpos = positionX*-1;
+          mpos = steps*-1;
         }
             stepper.move(mpos);
             stepper.setMaxSpeed(mspeed);
@@ -93,7 +134,7 @@ void moveto(){
                 break;
               }
             }
-        positionX=0;
+        steps=0;
         mspeed=0;
         mdir=0;
         datarvd=true;
@@ -111,8 +152,6 @@ void homestepper(){
     stepper.setMaxSpeed(3000.0);
     stepper.setAcceleration(3000.0);
 
-    //Serial.print("Pump motor is homing....");
-    //move in -x until endstop is pressed
     while (digitalRead(homeButton)){
       stepper.moveTo(init_homing);
       init_homing--;
@@ -124,13 +163,13 @@ void homestepper(){
     stepper.setAcceleration(3000.0);
     //Serial.print(init_homing);
     init_homing=1;
-    //move in +x until endstop is no longer pressed 
+
     while (!digitalRead(homeButton)){
       stepper.moveTo(init_homing);
       stepper.run();
       init_homing++;
     }
-    //set zero position
+
     stepper.setCurrentPosition(0);
     Serial.write("yup hom");  
 }
@@ -143,6 +182,24 @@ void changeaccel(){
             stepper.setAcceleration(maccel);
         maccel=0;
         datarvd=true;
+    }
+  }
+  datarvd=false;
+}
+void getdefaults(){
+  while (!datarvd){
+    if (Serial.readBytes(userInput,20)==20){
+      for (int i=1; i<10; i++){
+        defaultaccel=defaultaccel*10+userInput[i]-'0';
+      }
+      for (int j=11;j<20;j++){
+        defaultrate=defaultrate*10+userInput[j]-'0';
+      }
+      stepper.setAcceleration(defaultaccel);
+      stepper.setMaxSpeed(defaultrate);
+      defaultaccel=0;
+      defaultrate=0;
+      datarvd=true;
     }
   }
   datarvd=false;
